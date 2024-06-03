4bc17811199fb45d18b5d38c44ab1b8db1ea107b
Cheng WANG <cwang199@connect.hkust-gz.edu.cn>
2024-06-03 18:41:21 +0800

    it works! but not the code problem
    to see the lighthouse, you must remove the hat of BTSRC_SEL-VDDD

--- scm_v3c/applications/synclight_carlibration/synclight_calibration.c
+++ scm_v3c/applications/synclight_carlibration/synclight_calibration.c
@@ -35,7 +35,64 @@ extern int8_t need_optical;
 int t;
 
 //=========================== prototypes ======================================
+void config_lighthouse_mote(void){
+  // RF Timer rolls over at this value and starts a new cycle
+  RFTIMER_REG__MAX_COUNT = 0xFFFFFFFF;
+  // Enable RF Timer
+  RFTIMER_REG__CONTROL = 0x7;
+  // Disable interrupts for the radio FSM
+//  radio_disable_interrupts();
+  
+  // Disable RF timer interrupts
+//  rftimer_disable_interrupts();
+  // Init LDO control
+  init_ldo_control();
+  // Set LDO reference voltages
+  set_ALWAYSON_LDO_voltage(0);
+  // Select banks for GPIO inputs
+  GPI_control(0,0,0,0);
+  // Select banks for GPIO outputs
+  GPO_control(0,0,0,0);
+  // Set all GPIOs as outputs
+  GPI_enables(0x000F);//0008=io3?
+  GPO_enables(0xFFFF);
+  // Set HCLK source as HF_CLOCK
+//  set_asc_bit(1147);
+//  // Set initial coarse/fine on HF_CLOCK
+//  //coarse 0:4 = 860 861 875b 876b 877b
+//  //fine 0:4 870 871 872 873 874b
+////  set_sys_clk_secondary_freq(HF_CLOCK_coarse, HF_CLOCK_fine);
+//  
+//  // Set RFTimer source as HF_CLOCK
+//  set_asc_bit(1151);
+//  // Disable LF_CLOCK 
+//  set_asc_bit(553);
+//  // Set HCLK divider to 2
+//  clear_asc_bit(57);
+//  clear_asc_bit(56);
+//  clear_asc_bit(55);
+//  clear_asc_bit(54);
+//  clear_asc_bit(53);
+//  set_asc_bit(52);//inverted
+//  set_asc_bit(51);
+//  clear_asc_bit(50);
+//  // Set RF Timer divider to pass through so that RF Timer is 20 MHz
+//  set_asc_bit(36);
 
+  // Set 2M RC as source for chip CLK
+  set_asc_bit(1156);
+  // Enable passthrough on chip CLK divider
+  set_asc_bit(41);
+
+  // Init counter setup - set all to analog_cfg control
+  // ASC[0] is leftmost
+  //ASC[0] |= 0x6F800000; 
+  for(t=2; t<9; t++) set_asc_bit(t);
+  analog_scan_chain_write();
+	analog_scan_chain_load();
+  
+//  ANALOG_CFG_REG__10 = 0x58;
+}
 //=========================== main ============================================
 
 int main(void) {
@@ -44,29 +101,31 @@ int main(void) {
     memset(&app_vars, 0, sizeof(app_vars_t));
 
     printf("Initializing...");
-
+//config_lighthouse_mote();
     initialize_mote();
     crc_check();
     perform_calibration();
     printf("~~~~my code start~~~~~%d\n", app_vars.count);
+  
+    config_lighthouse_mote();
     //		gpio_init();
 
     // gpio9 = 0x0200
-    GPO_control(6, 6, 6, 6);  // bank6 is the GPIO port out
-    analog_scan_chain_write();
-    analog_scan_chain_load();
-    GPO_enables(0x0000);
-    GPO_enables(1 << CALIBRATE_OUTPUT);  // IO 10,0x0400
+//    GPO_control(6, 6, 6, 6);  // bank6 is the GPIO port out
+//    analog_scan_chain_write();
+//    analog_scan_chain_load();
+//    GPO_enables(0x0000);
+//    GPO_enables(1 << CALIBRATE_OUTPUT);  // IO 10,0x0400
 
     //	GPI_enables(0x0008);//IO 3
-    GPI_enables(1 << CALIBRATE_SYNCLIGHT_INPUT);  // IO 8
-    //	GPI_enables(0x0200);//IO 9
-    analog_scan_chain_write();
-    analog_scan_chain_load();
+//    GPI_enables(1 << CALIBRATE_SYNCLIGHT_INPUT);  // IO 8
+//    //	GPI_enables(0x0200);//IO 9
+//    analog_scan_chain_write();
+//    analog_scan_chain_load();
 
     // gpio_3_set();
-    printf("~~~~enable INTs~~~~~%d\n", app_vars.count);
-    gpio_ext_3_interrupt_enable();
+//    printf("~~~~enable INTs~~~~~%d\n", app_vars.count);
+//    gpio_ext_3_interrupt_enable();
     // clean optical and ex3 interrupt, then re-open ext_3 interrupt
     need_optical = 0;
     //		enable extern interrupts and first clean EXT_8 and optical
@@ -80,15 +139,15 @@ int main(void) {
     //    ICER = 0xFFFF;
     //    GPO_control(0,6,6,6);// bank6 is the GPIO port out
     //    GPI_control(0,6,6,6);// bank6 is the GPIO port out
-    GPO_control(0, 6, 0, 0);  // bank6 is the GPIO port out
-    GPI_control(0, 0, 1, 0);  // bank6 is the GPIO port out
-    analog_scan_chain_write();
-    analog_scan_chain_load();
-    //    GPI_enables(0x0008);//IO 3
-    GPI_enables(0xFF08);  // IO 0-3
-    GPO_enables(0xD0FF);  // IO 0-3
-    analog_scan_chain_write();
-    analog_scan_chain_load();
+//    GPO_control(0, 6, 0, 0);  // bank6 is the GPIO port out
+//    GPI_control(0, 0, 1, 0);  // bank6 is the GPIO port out
+//    analog_scan_chain_write();
+//    analog_scan_chain_load();
+//    //    GPI_enables(0x0008);//IO 3
+//    GPI_enables(0xFF08);  // IO 0-3
+//    GPO_enables(0xD0FF);  // IO 0-3
+//    analog_scan_chain_write();
+//    analog_scan_chain_load();
 
 //    ICER = 0xFFFF;
     //	gpio_3_set();
@@ -113,8 +172,8 @@ int main(void) {
 //    // Disable LF_CLOCK
 //    set_asc_bit(553);
 
-    analog_scan_chain_write();
-    analog_scan_chain_load();
+//    analog_scan_chain_write();
+//    analog_scan_chain_load();
 
     printf("~~~~start to say HELLO?~~~~~%d\n", app_vars.count);
     while (1) {
